5a316e1390caa0d63fbc527ce03b4b12ad82d786
biancabeatriz47 <chidnoblap444@gmail.com>
2026-05-10 21:41:15 +0800

    add enqueue, isEmpty, size to ServiceQueue

--- queue/ServiceQueue.cpp
+++ queue/ServiceQueue.cpp
@@ -1,1 +1,42 @@
 // Task 4: Ferenal
+#include "ServiceQueue.h"
+#include <iostream>
+#include <iomanip>
+ 
+QueueNode::QueueNode(const Transaction& t)
+    : data(t), next(nullptr) {}
+ 
+ServiceQueue::ServiceQueue()
+    : front_(nullptr), rear_(nullptr), count_(0) {}
+ 
+ServiceQueue::~ServiceQueue() {}
+ 
+void ServiceQueue::enqueue(const Transaction& transaction) {
+    QueueNode* newNode = new QueueNode(transaction);
+ 
+    if (isEmpty()) {
+        front_ = newNode;
+        rear_  = newNode;
+    } else {
+        rear_->next = newNode;
+        rear_       = newNode;
+    }
+ 
+    ++count_;
+ 
+    std::cout << "\n[QUEUE] Customer added to line.\n"
+              << "  Account : " << transaction.getAccountNumber() << "\n"
+              << "  Type    : " << transaction.getType()          << "\n"
+              << "  Amount  : PHP "
+              << std::fixed << std::setprecision(2) << transaction.getAmount() << "\n"
+              << "  Position in line: " << count_ << "\n";
+}
+ 
+bool ServiceQueue::isEmpty() const {
+    return count_ == 0;
+}
+ 
+int ServiceQueue::size() const {
+    return count_;
+}
+=
--- queue/ServiceQueue.h
+++ queue/ServiceQueue.h
@@ -1,1 +1,30 @@
 // Task 4: Ferenal
+#pragma once
+#include <string>
+#include "../transaction/Transaction.h"
+ 
+struct QueueNode {
+    Transaction data;
+    QueueNode*  next;
+ 
+    explicit QueueNode(const Transaction& t);
+};
+ 
+class ServiceQueue {
+public:
+    ServiceQueue();
+    ~ServiceQueue();
+ 
+    ServiceQueue(const ServiceQueue&)            = delete;
+    ServiceQueue& operator=(const ServiceQueue&) = delete;
+ 
+    void enqueue(const Transaction& transaction);
+    bool isEmpty() const;
+    int  size()    const;
+ 
+private:
+    QueueNode* front_;
+    QueueNode* rear_;
+    int        count_;
+};
+ 
