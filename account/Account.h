abb62fd49e7237e923bc9381f4da1bc122d07efc
johnkevinrepolidon <johnkevinrepolidon@gmail.com>
2026-05-13 02:05:59 +0800

    Account.h adjustments

--- account/Account.h
+++ account/Account.h
@@ -1,1 +1,25 @@
 // Task 1: Repolidon
+
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <string>
+
+class Account {
+private:
+    std::string id;
+    std::string name;
+    double bal;
+
+public:
+    Account(std::string accId, std::string accName, double initial);
+    
+    void add(double amt);
+    bool sub(double amt);
+    
+    double getBal() const;
+    std::string getId() const;
+    std::string getName() const;
+};
+
+#endif
